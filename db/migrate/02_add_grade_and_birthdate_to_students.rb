12d19509c78a91d6ccc83a4a617ba3c9470bdef5
Anthony Leon <acidknights89@gmail.com>
2020-11-20 04:27:04 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++
@@ -1,6 +0,0 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord[5.1]::Migration
-  def change
-    add_column :students, :grade, :integer
-    add_column :students, :birthdate, :string
-  end
-end
